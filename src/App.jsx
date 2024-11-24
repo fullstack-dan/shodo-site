98f7dc865c602e2dc668fb413e97916cf6ffe791
fullstack-dan <aouloudan@icloud.com>
2024-11-24 14:44:47 -0500

    added landing page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <main>
+                <h1>
+                    <a href="https://x.com/shodoapp">Shodo</a>
+                </h1>
+                <h2>
+                    coming soon<span className="dots"></span>
+                </h2>
+            </main>
+        </>
+    );
+}
+
+export default App;
